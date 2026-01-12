785842f3cb74507e9fcf01d8f0d7638380c56624
Adarsh <Mr.aadarshkumarsingh@gmail.com>
2026-01-12 17:46:06 +0530

    fix

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 
 import ComposeJob from "../Components/ComposeJob";
 import api from "../api/client";
