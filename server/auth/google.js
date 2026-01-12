dad366fe5311a5548db0239d27351da44d2f761d
Adarsh <Mr.aadarshkumarsingh@gmail.com>
2026-01-12 16:32:59 +0530

    change

--- server/auth/google.js
+++ server/auth/google.js
@@ -6,7 +6,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || "/auth/google/callback",
       scope: ["profile", "email", "https://www.googleapis.com/auth/gmail.readonly"]
     },
     async (accessToken, refreshToken, profile, done) => {
